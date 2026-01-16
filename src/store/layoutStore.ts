10bb74beeaeff9221b114089ea076ff3c3293d7e
Thomas Schlender <thebuoyant@gmail.com>
2026-01-16 17:59:38 +0100

    RHN-3 Improve layout store

--- src/store/layoutStore.ts
+++ src/store/layoutStore.ts
@@ -6,11 +6,19 @@ type LayoutState = {
   cardWidth: number;
   cardSpace: number;
   branchHeight: number;
+  showHeader: boolean;
+  showLayerA: boolean;
+  showLayerB: boolean;
+  showLayerC: boolean;
 
   setCardHeight: (cardHeight: number) => void;
   setCardWidth: (cardWidth: number) => void;
   setCardSpace: (cardSpace: number) => void;
   setBranchHeight: (branchHeight: number) => void;
+  setShowHeader: (showHeader: boolean) => void;
+  setShowLayerA: (showLayerA: boolean) => void;
+  setShowLayerB: (showLayerB: boolean) => void;
+  setShowLayerC: (showLayerC: boolean) => void;
 };
 
 export const useLayoutStore = create<LayoutState>((set) => ({
@@ -18,10 +26,17 @@ export const useLayoutStore = create<LayoutState>((set) => ({
   cardWidth: APP_CONFIG.layout.card.cardWidth,
   cardSpace: APP_CONFIG.layout.card.cardSpace,
   branchHeight: 48,
+  showHeader: true,
+  showLayerA: true,
+  showLayerB: true,
+  showLayerC: true,
 
-  setCardHeight: (receivedValue: number) => set({ cardHeight: receivedValue }),
-  setCardWidth: (receivedValue: number) => set({ cardWidth: receivedValue }),
-  setCardSpace: (receivedValue: number) => set({ cardSpace: receivedValue }),
-  setBranchHeight: (receivedValue: number) =>
-    set({ branchHeight: receivedValue }),
+  setCardHeight: (value: number) => set({ cardHeight: value }),
+  setCardWidth: (value: number) => set({ cardWidth: value }),
+  setCardSpace: (value: number) => set({ cardSpace: value }),
+  setBranchHeight: (value: number) => set({ branchHeight: value }),
+  setShowHeader: (value: boolean) => set({ showHeader: value }),
+  setShowLayerA: (value: boolean) => set({ showLayerA: value }),
+  setShowLayerB: (value: boolean) => set({ showLayerB: value }),
+  setShowLayerC: (value: boolean) => set({ showLayerC: value }),
 }));
