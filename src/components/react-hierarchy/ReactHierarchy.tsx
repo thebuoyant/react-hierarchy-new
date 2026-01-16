7096fbf68092f74f17139c32dde195360944840c
Thomas Schlender <thebuoyant@gmail.com>
2026-01-16 17:26:06 +0100

    RHN-2 Add base layer wrappers

--- src/components/react-hierarchy/ReactHierarchy.tsx
+++ src/components/react-hierarchy/ReactHierarchy.tsx
@@ -8,12 +8,26 @@ export type ReactHierarchyProps = {
 };
 
 export default function ReactHierarchy({
-  titleType = "h3",
+  titleType = "h5",
   appConfig = APP_CONFIG,
 }: ReactHierarchyProps) {
   return (
     <div className="react-hierarchy">
-      <div className="layout">layout</div>
+      <div className="layout-wrapper">
+        <div className="header-wrapper">header</div>
+        <div className="layer-a-wrapper">
+          <div className="layer-a-branch-wrapper">layer-a-branch</div>
+          <div className="layer-a-cards-wrapper">layer-a-cards</div>
+        </div>
+        <div className="layer-b-wrapper">
+          <div className="layer-b-branch-wrapper">layer-b-branch</div>
+          <div className="layer-b-cards-wrapper">layer-b-cards</div>
+        </div>
+        <div className="layer-c-wrapper">
+          <div className="layer-c-branch-wrapper">layer-c-branch</div>
+          <div className="layer-c-cards-wrapper">layer-c-cards</div>
+        </div>
+      </div>
     </div>
   );
 }
