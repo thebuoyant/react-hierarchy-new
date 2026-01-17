d29431db07c123a8135119661b26413c34bed1fa
Thomas Schlender <thebuoyant@gmail.com>
2026-01-17 19:06:46 +0100

    RHN-12 Render cards for layer a

--- src/components/layer-a-cards/LayerACards.tsx
+++ src/components/layer-a-cards/LayerACards.tsx
@@ -1,3 +1,33 @@
+import { useDataStore } from "../../store/dataStore";
+import { HierarchyNodeType } from "../../types/hierarchy-node.types";
+import Card from "../card/Card";
+
 export default function LayerACards() {
-  return <div className="layer-a-cards">layer a cards</div>;
+  // data store
+  const dataLayerA = useDataStore((s) => s.dataLayerA);
+
+  return (
+    <div className="layer-a-cards">
+      {dataLayerA.map((node: HierarchyNodeType, index: number) => {
+        const content = <div>some content layer a</div>;
+        const handleNavButtonClick = () => {
+          const selectedNodeId = node.id;
+
+          console.log("selectedNodeId", selectedNodeId);
+        };
+        return (
+          <Card
+            key={index}
+            node={node}
+            showNavButton={node.children.length > 0}
+            showParentConnection={false}
+            showChildConnection={false}
+            positionIndex={index}
+            content={content}
+            onNavButtonClick={handleNavButtonClick}
+          />
+        );
+      })}
+    </div>
+  );
 }
