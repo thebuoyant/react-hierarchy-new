f8e6af337aedff1ebb3b57d86ad58b0a4552570b
Thomas Schlender <thebuoyant@gmail.com>
2026-01-17 17:01:12 +0100

    RHN-6 Improve data store

--- src/store/dataStore.ts
+++ src/store/dataStore.ts
@@ -4,12 +4,26 @@ import { DEFAULT_MOCK_DATA } from "../_mock-data/default-mock-data";
 
 type DataState = {
   data: HierarchyNodeType;
+  dataLayerA: HierarchyNodeType[];
+  dataLayerB: HierarchyNodeType[];
+  dataLayerC: HierarchyNodeType[];
+
   setData: (data: HierarchyNodeType) => void;
   resetData: () => void;
+  setDataLayerA: (dataLayerA: HierarchyNodeType[]) => void;
+  setDataLayerB: (dataLayerB: HierarchyNodeType[]) => void;
+  setDataLayerC: (dataLayerC: HierarchyNodeType[]) => void;
 };
 
 export const useDataStore = create<DataState>((set) => ({
   data: DEFAULT_MOCK_DATA,
+  dataLayerA: [],
+  dataLayerB: [],
+  dataLayerC: [],
+
   setData: (receivedValue) => set({ data: receivedValue }),
   resetData: () => set({ data: DEFAULT_MOCK_DATA }),
+  setDataLayerA: (value: HierarchyNodeType[]) => set({ dataLayerA: value }),
+  setDataLayerB: (value: HierarchyNodeType[]) => set({ dataLayerB: value }),
+  setDataLayerC: (value: HierarchyNodeType[]) => set({ dataLayerC: value }),
 }));
